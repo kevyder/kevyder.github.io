210a4ae84a852929451c6cf6bd7c8aaa79e270f1
Kevin Rodríguez <kyrodriguez97@gmail.com>
2017-06-02 12:47:55 -0500

    Projects session

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var app = angular.module("app", []);
+
+app.controller("AppCtrl", function($scope, $http) {
+  $http.get('https://api.github.com/users/kevyder/repos?per_page=50').
+  success(function(data) {
+    var repos = [];
+    data.forEach(function(record) {
+      if (!(record.fork)) {
+        repos.push(record);
+      }
+    });
+    console.log(repos);
+    $scope.github = repos;
+    var options = [];
+    data.forEach(function(record) {
+      if (options.indexOf(record.language) == -1 && record.language) {
+        options.push(record.language);
+      }
+    });
+    $scope.options = options;
+  }).error(function(data) {
+    // log error
+  });
+});
