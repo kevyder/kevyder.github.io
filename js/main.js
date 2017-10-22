ed8f5698f51040bbaf260a31f762e475d4949fc0
Kevin <kyrodriguez97@gmail.com>
2017-10-22 13:03:47 -0500

    Adding responsive navbar

--- js/main.js
+++ js/main.js
@@ -70,3 +70,17 @@ function repoTemplate(repos) {
   }
   project.innerHTML = repoHTML;
 }
+
+let mobileButton = document.querySelector('.mobile-button');
+
+mobileButton.addEventListener('click', () => {
+  let cO = document.querySelector('.container-options'),
+      lines = mobileButton.children[0];
+  if(cO.classList.contains('active-menu')){
+    cO.classList.remove('active-menu');
+    lines.classList.remove('close-lines');
+  }else{
+    cO.classList.add('active-menu');
+    lines.classList.add('close-lines');
+  }
+});
