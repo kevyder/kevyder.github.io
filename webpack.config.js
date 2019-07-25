67798a2aa96861bad98af4eb7d9e1fca7a9eb877
Kevin <kyrodriguez97@gmail.com>
2019-07-25 18:53:46 -0500

    Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,34 @@
-// webpack.config.js
+const path = require('path');
+
+module.exports = {
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	entry: ['./js/app.js', './sass/app.sass'],
+	output: {
+		path: path.resolve(__dirname, 'assets'),
+		filename: 'js/app.min.js',
+	},
+	module: {
+		rules: [{
+			test: /(\.css|\.scss|\.sass)$/,
+			use: [{
+					loader: 'file-loader',
+					options: {
+						name: 'css/[name].min.css',
+					}
+				},
+				{
+					loader: 'extract-loader'
+				},
+				{
+					loader: 'css-loader?-url'
+				},
+				{
+					loader: 'postcss-loader'
+				},
+				{
+					loader: 'sass-loader'
+				}
+			]
+		}]
+	}
+};
